d0fa7edde0b14419e31b99bd4726ae79af40214f
Reto Lamprecht <reto.lamprecht@gmx.ch>
2020-07-19 21:03:38 +0200

    improved demo for canvas resize

--- src/demos/canvas-resize-demo.ts
+++ src/demos/canvas-resize-demo.ts
@@ -1,13 +1,13 @@
-import { Dimensions } from "@comicvm-geometry-2d/Dimensions";
-import { Point } from "@comicvm-geometry-2d/Point";
-import { Rectangle } from "@comicvm-geometry-2d/Rectangle";
-import { Canvas } from "src/dom/Canvas";
-import { Div } from "src/dom/Div";
-import { InputType } from "src/dom/Input";
-import { InputWithLabel } from "src/dom/InputWithLabel";
-import { Font } from "src/dom/util/Font";
-import { PaintStyle } from "src/dom/util/PaintStyle";
-import { TextAlign } from "src/dom/util/TextAlign";
+import {Dimensions} from "@comicvm-geometry-2d/Dimensions";
+import {Point} from "@comicvm-geometry-2d/Point";
+import {Rectangle} from "@comicvm-geometry-2d/Rectangle";
+import {Canvas} from "src/dom/Canvas";
+import {Div} from "src/dom/Div";
+import {InputType} from "src/dom/Input";
+import {InputWithLabel} from "src/dom/InputWithLabel";
+import {Font} from "src/dom/util/Font";
+import {PaintStyle} from "src/dom/util/PaintStyle";
+import {TextAlign} from "src/dom/util/TextAlign";
 
 export function createCanvasResizeDemo(): Div {
 
@@ -17,7 +17,7 @@ export function createCanvasResizeDemo(): Div {
         width: canvasDimensions.width,
         height: canvasDimensions.height,
         scale: 1.0,
-        paintStyleConfig: { font: new Font(30, "Arial") }
+        paintStyleConfig: {font: new Font(30, "Arial")}
     };
 
     const canvasResizable = Canvas.create(canvasConfig)
@@ -45,7 +45,7 @@ export function createCanvasResizeDemo(): Div {
         canvas.text(
             text,
             new Point(150, 110),
-            PaintStyle.create({ fillStyle: "navy", textAlign: TextAlign.Center })
+            PaintStyle.create({fillStyle: "navy", textAlign: TextAlign.Center})
         );
     }
 
@@ -71,16 +71,18 @@ export function createCanvasResizeDemo(): Div {
     canvasResizable.onClick = createClickHandler(canvasResizable);
     canvasFixed.onClick = createClickHandler(canvasFixed);
 
-    return Div.create({ styleClass: "demo-section" })
+    return Div.create({styleClass: "demo-section"})
         .append("<h2>Canvas Size and Scale</h2>")
         .append(canvasFixed)
         .append("The lower canvas' scale keeps adjusting to fit the content of the upper canvas.")
         .append(canvasResizable)
         .append(widthInput)
         .append(heightInput)
-        .append(Div.create({ styleClass: "code-sample" })
+        .append(Div.create({styleClass: "code-sample"})
             .append(`
 <pre>
+// 1. Create two identical canvas elements 
+
 const canvasDimensions: Dimensions = new Dimensions(300, 200);
 
 const canvasConfig = {
@@ -93,9 +95,12 @@ const canvasConfig = {
 const canvasResizable = Canvas.create(canvasConfig)
 const canvasFixed = Canvas.create(canvasConfig)
 
-const widthInput = InputWithLabel.create({ type: InputType.NUMBER, text: "Canvas width", value: canvasResizable.width });
-const heightInput = InputWithLabel.create({ type: InputType.NUMBER, text: "Canvas height", value: canvasResizable.height });
+paintOnCanvas(canvasResizable, "300px * 200px");
+paintOnCanvas(canvasFixed, "300px * 200px");
 
+/**
+ * Paints a magenta circle, an orange rectangle and some blue text.
+ */
 function paintOnCanvas(canvas: Canvas, text: string) {
     canvas.circle(new Point(150, 100), 80, PaintStyle.stroke("magenta"));
     canvas.rect(Rectangle.fromDimensions(canvasDimensions).cutMarginOf(1), PaintStyle.stroke("orange", 1));
@@ -106,18 +111,30 @@ function paintOnCanvas(canvas: Canvas, text: string) {
     );
 }
 
+
+// 2. Allow users to resize the second canvas using two input fields (widthInput, heightInput).
+
+const widthInput = InputWithLabel.create({ type: InputType.NUMBER, text: "Canvas width", value: canvasResizable.width });
+const heightInput = InputWithLabel.create({ type: InputType.NUMBER, text: "Canvas height", value: canvasResizable.height });
+
+widthInput.onChange = repaint;
+heightInput.onChange = repaint;
+
+/**
+ * Resizes the resizable canvas to the values in the widthInput and heightInput fields.
+ */
 function repaint(e) {
     canvasResizable.setDimensions(parseInt(widthInput.value), parseInt(heightInput.value));
     canvasResizable.transformTo(canvasFixed.shape.translateToOrigin());
     paintOnCanvas(canvasResizable, \`\${canvasResizable.width}px * \${canvasResizable.height}px\`);
 };
 
-widthInput.onChange = repaint;
-heightInput.onChange = repaint;
 
-paintOnCanvas(canvasResizable, "300px * 200px");
-paintOnCanvas(canvasFixed, "300px * 200px");
+// 3. Demonstrate coordinate transformation from mouse/screen coordinates to canvas coordinates for any canvas size.
 
+/**
+ * Paints a green dot at the position where the mouse was clicked.
+ */
 function createClickHandler(canvas) {
     return (e: MouseEvent) => {
         const pos = canvas.getCanvasPositionFromMousePosition(e.clientX, e.clientY);
